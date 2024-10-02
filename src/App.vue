ca7d334ba7fda27dd67e165d203bc20e5bad635f
HanyMohamed <hsaad9231@gmail.com>
2024-10-02 13:45:40 +0300

    🎉init(starting project ): first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <h1 class="text-3xl text-red-600 font-bold underline">Hello world</h1>
+</template>
+
+<style scoped></style>
